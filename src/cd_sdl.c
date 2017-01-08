79173995c0c01bbd06b1b4c54ca24cdc0af26b56
Ranger <ranger@quakecodex.com>
2017-01-08 18:42:35 -0500

    Finished documenting source code

--- src/cd_sdl.c
+++ src/cd_sdl.c
@@ -8,40 +8,47 @@ of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-
-See the GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+Quake is a trademark of Id Software, Inc., (c) 1996 Id Software, Inc. All
+rights reserved.
 */
-// Quake is a trademark of Id Software, Inc., (c) 1996 Id Software, Inc. All
-// rights reserved.
+
+/** 
+ * \file cd_sdl.c
+ * Functions for playing CD music.
+ * 
+ * @author: James Johnson
+ * @version: 0.1.2 2017-1-8
+ */
 
 #include "quakedef.h"
 #include "sdlquake.h"
 
 extern	cvar_t	bgmvolume;
 
-static qboolean cdValid = false;
-static qboolean	playing = false;
+static qboolean cdValid = false; /**< True if there is a CD in the drive. */
+static qboolean	playing = false; /**< True when the CD is playing. */
+/** True if the CD was playing before being paused, not stopped. Used to
+track when resume command will work. */
 static qboolean	wasPlaying = false;
-static qboolean	initialized = false;
-static qboolean	enabled = false;
-static qboolean playLooping = false;
-static float	cdvolume;
-static byte 	remap[100];
-static byte		playTrack;
-static byte		maxTrack;
-
-static SDL_CD *cdinfo;
-
-UINT	wDeviceID;
+static qboolean	initialized = false; /**< True when CD-ROM drive is initialized. */
+/** True when CD-ROM drive is enabled and ready to play. */
+static qboolean	enabled = false; 
+static qboolean playLooping = false; /**< True if current track should be looped. */
+static float	cdvolume; /**< CD Music volume. (0 or 1) */
+static byte 	remap[100]; /**< Track lookup table. */
+static byte		playTrack; /**< Track number of playing track. */
+static byte		maxTrack; /**< Maximum tracks on the CD. */
+static SDL_CD *cdinfo; /**< SDL CD-ROM structure. */
 
 /**
- * Ejects the CD 
+ * Ejects the CD.
  */
 static void CDAudio_Eject(void)
 {
@@ -67,7 +74,7 @@ static int CDAudio_GetAudioDiskInfo(void)
 	cdValid = false;
 
 	if (!cdinfo) {
-		cdinfo = SDL_CDOpen(wDeviceID);
+		cdinfo = SDL_CDOpen(0);
 		if (!cdinfo)
 		{
 			Con_DPrintf("CDAudio: drive not ready\n");
@@ -92,19 +99,21 @@ static int CDAudio_GetAudioDiskInfo(void)
 	return 0;
 }
 
-
 /**
  * Plays a CD track.
- * @param track		The track number to play starting with 2. The 1st track on the Quake CD is reserved for the game data. Vanilla Quake has 10 tracks in total, numbered 2-11.
- * @param looping	True to loop the music, false to play once. Note that this does not work on all systems. 
+ * @param track		The track number to play starting with 2. The 1st track on 
+ *					the Quake CD is reserved for the game data. Vanilla Quake 
+ *					has 10 tracks in total, numbered 2-11.
+ * @param looping	True to loop the music, false to play once. Note that this 
+ *					does not work on all systems. 
  */
 void CDAudio_Play(byte track, qboolean looping)
 {
 	int dwReturn;
-	int trackLength;
 	CDstatus status;
 
-	/* Check that the CD-ROM drive is initialized and there is a CD in the tray. */
+	/* Check that the CD-ROM drive is initialized and there is a CD 
+	in the tray */
 	if (!enabled)
 		return;
 	
@@ -114,11 +123,19 @@ void CDAudio_Play(byte track, qboolean looping)
 		if (!cdValid)
 			return;
 	}
-
-	/* SDL ignores data tracks, so it always starts at 1. The track number needs to be adjusted to work with SDL. */
+	
+	/* SDL ignores data tracks, so it always starts at 1. The track number  
+	needs to be adjusted to work with SDL. */
 	track = remap[track];
 
-	status = SDL_CDStatus(cdinfo); /* Remove? */s
+	/* Skip if the track is already playing. */
+	status = SDL_CDStatus(cdinfo);
+	if (status == CD_PLAYING) 
+	{
+		if (track == playTrack)
+			return;
+		CDAudio_Stop();
+	}
 
 	/* Check to make sure the track is valid. */
 	if (track < 1 || track > maxTrack)
@@ -128,26 +145,26 @@ void CDAudio_Play(byte track, qboolean looping)
 	}
 	
 	/* Play the track */
-	dwReturn = SDL_CDPlayTracks(cdinfo, track, 0, 0, cdinfo->track[track].length);
+	dwReturn = SDL_CDPlay(cdinfo, cdinfo->track[track].offset, cdinfo->track[track].length);
 	if (dwReturn < 0)
 	{
 		Con_DPrintf("SDL_CDPlayTracks failed (%i)\n", dwReturn);
 		return;
 	}
-
-	// get the length of the track to be played
-	trackLength = cdinfo->track[track].length;
 	
 	playLooping = looping;
 	playTrack = track;
 	playing = true;
 
-	/* There's no volume control for CD music, so pause the CD if the volume is off. */
+	/* There's no volume control for CD music, so pause the CD if the
+	volume is off. */
 	if (cdvolume == 0.0)
 		CDAudio_Pause ();
 }
 
-
+/**
+ * Stops CD playback.
+ */
 void CDAudio_Stop(void)
 {
 	if (!enabled)
@@ -164,6 +181,9 @@ void CDAudio_Stop(void)
 }
 
 
+/**
+ * Pauses CD playback.
+ */
 void CDAudio_Pause(void)
 {
 	if (!enabled)
@@ -179,7 +199,9 @@ void CDAudio_Pause(void)
 	playing = false;
 }
 
-
+/**
+ * Resumes playback after a pause.
+ */
 void CDAudio_Resume(void)
 {
 	if (!enabled)
@@ -199,7 +221,9 @@ void CDAudio_Resume(void)
 	playing = true;
 }
 
-
+/**
+ * Registers CD console commands.
+ */
 static void CD_f (void)
 {
 	char	*command;
@@ -211,12 +235,14 @@ static void CD_f (void)
 
 	command = Cmd_Argv (1);
 
+	/* Turn CD music on */
 	if (Q_strcasecmp(command, "on") == 0)
 	{
 		enabled = true;
 		return;
 	}
 
+	/* Turn CD music off */
 	if (Q_strcasecmp(command, "off") == 0)
 	{
 		if (playing)
@@ -225,6 +251,7 @@ static void CD_f (void)
 		return;
 	}
 
+	/* Reset CD Music to defaults */
 	if (Q_strcasecmp(command, "reset") == 0)
 	{
 		enabled = true;
@@ -236,6 +263,7 @@ static void CD_f (void)
 		return;
 	}
 
+	/* Remap CD track to user entered number */
 	if (Q_strcasecmp(command, "remap") == 0)
 	{
 		ret = Cmd_Argc() - 2;
@@ -251,6 +279,7 @@ static void CD_f (void)
 		return;
 	}
 
+	/* Close the CD-ROM drive door */
 	if (Q_strcasecmp(command, "close") == 0)
 	{
 		CDAudio_CloseDoor();
@@ -267,36 +296,42 @@ static void CD_f (void)
 		}
 	}
 
+	/* Play a track */
 	if (Q_strcasecmp(command, "play") == 0)
 	{
 		CDAudio_Play((byte)Q_atoi(Cmd_Argv (2)), false);
 		return;
 	}
 
+	/* Loop a track; 1 = loop, 0 = play once */
 	if (Q_strcasecmp(command, "loop") == 0)
 	{
 		CDAudio_Play((byte)Q_atoi(Cmd_Argv (2)), true);
 		return;
 	}
 
+	/* Stop playing a track */
 	if (Q_strcasecmp(command, "stop") == 0)
 	{
 		CDAudio_Stop();
 		return;
 	}
 
+	/* Pause the current track */
 	if (Q_strcasecmp(command, "pause") == 0)
 	{
 		CDAudio_Pause();
 		return;
 	}
 
+	/* Resume playing */
 	if (Q_strcasecmp(command, "resume") == 0)
 	{
 		CDAudio_Resume();
 		return;
 	}
 
+	/* Not implemented */
 	if (Q_strcasecmp(command, "eject") == 0)
 	{
 		if (playing)
@@ -306,23 +341,37 @@ static void CD_f (void)
 		return;
 	}
 
+	/* Print out CD info */
 	if (Q_strcasecmp(command, "info") == 0)
 	{
 		Con_Printf("%u tracks\n", maxTrack);
-		if (playing)
-			Con_Printf("Currently %s track %u\n", playLooping ? "looping" : "playing", playTrack);
-		else if (wasPlaying)
-			Con_Printf("Paused %s track %u\n", playLooping ? "looping" : "playing", playTrack);
 		Con_Printf("Volume is %f\n", cdvolume);
+		if (playing)
+		{
+			Con_Printf("Currently %s track %u\n", 
+						playLooping ? "looping" : "playing", 
+						playTrack);
+		}
+		else if (wasPlaying)
+		{
+			Con_Printf("Paused %s track %u\n", 
+						playLooping ? "looping" : "playing", 
+						playTrack);
+		}
 		return;
 	}
 }
 
+/**
+ * Called once per frame. Checks for volume changes and restarts playback 
+ * on looping tracks.
+ */
 void CDAudio_Update(void)
 {
 	if (!enabled)
 		return;
 
+	/* Set volume */
 	if (bgmvolume.value != cdvolume)
 	{
 		if (cdvolume)
@@ -338,6 +387,8 @@ void CDAudio_Update(void)
 			CDAudio_Resume ();
 		}
 	}
+	
+	/* Check if current track needs to be restarted. */
 	if (playLooping) 
 	{
 		if ((SDL_CDStatus(cdinfo) != CD_PLAYING) && (SDL_CDStatus(cdinfo) != CD_PAUSED)) 
@@ -350,6 +401,7 @@ void CDAudio_Update(void)
 
 /**
  * Initializes the CD-ROM drive and gets track info.
+ * @return	0 when initialization succeeds. -1 if there was an error.
  */
 int CDAudio_Init(void)
 {
@@ -373,8 +425,6 @@ int CDAudio_Init(void)
 		return -1;
 	}
 
-	wDeviceID = 0;
-
 	for (n = 0; n < 100; n++)
 		remap[n] = n - 1;
 	initialized = true;
@@ -389,7 +439,7 @@ int CDAudio_Init(void)
 
 
 /**
- * Stops audio, closes the CD and shuts down SDL CD.
+ * Stops audio, closes the CD drive and shuts down SDL CD.
  */
 void CDAudio_Shutdown(void)
 {
--- src/cdaudio.h
+++ src/cdaudio.h
@@ -15,13 +15,54 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
 */
 
+/** 
+ * \file cdaudio.h
+ * Functions for playing CD music.
+ * 
+ * @author: James Johnson
+ * @version: 0.1.2 2017-1-8
+ */
+
+/**
+ * Initializes the CD-ROM drive and gets track info.
+ * @return	0 when initialization succeeds. -1 if there was an error.
+ */
 int CDAudio_Init(void);
+
+/**
+ * Plays a CD track.
+ * @param track		The track number to play starting with 2. The 1st track on 
+ *					the Quake CD is reserved for the game data. Vanilla Quake 
+ *					has 10 tracks in total, numbered 2-11.
+ * @param looping	True to loop the music, false to play once. Note that this 
+ *					does not work on all systems. 
+ */
 void CDAudio_Play(byte track, qboolean looping);
+
+/**
+ * Stops CD playback.
+ */
 void CDAudio_Stop(void);
+
+/**
+ * Pauses CD playback.
+ */
 void CDAudio_Pause(void);
+
+/**
+ * Resumes playback after a pause.
+ */
 void CDAudio_Resume(void);
+
+/**
+ * Stops audio, closes the CD drive and shuts down SDL CD.
+ */
 void CDAudio_Shutdown(void);
+
+/**
+ * Called once per frame. Checks for volume changes and restarts playback 
+ * on looping tracks.
+ */
 void CDAudio_Update(void);
